c148c8bc4fdd591744cc8bae3df73514b75afe46
T. Ryan Johnson <johnsontr@gmail.com>
2022-12-03 17:32:23 -0600

    Update me.m
    Add testing case. Begin building the mean and vcov.

--- me.m
+++ me.m
@@ -1,31 +1,96 @@
 
-% MEAN
-% Dx1 vector
+function [ mean_vec, var_mat ] = me(hyp, meanfunc, covfunc, X, y, xs)
 
-% VARIANCE
-% 1. DxD
-% 2. Dxn <--- same as (4) transposed
-% 3. nxn
-% 4. nxD <--- same as (2) transposed
+% Build the mean and variance of the GPME. Output should be a Dx1 vector
+% and a DxD matrix for any input xs. Then call this function to generate on
+% the sample or on a predict plot.
 
-n = size(X,1);
+% input: hyp, meanfunc, covfunc, X, y, xs
+%   X and y must be normalized.
 
-mean = foo;
+% Add a "data=" option where certain things are calculated automatically.
+% Split a grid automatically with a reasonable number of points based on
+% the range.
 
-MODE = 'iso';
 
-% Switch length scale for the derivative depending on if the kernel is SEiso or SEard
-LS_SWITCH = 0;
-if strcmp(MODE,'iso')
-    LS_SWITCH = 1; % For ISO
-elseif strcmp(MODE,'ard')
-    LS_SWITCH = d;
+%%%
+% Support variables for testing
+n=50; % Number of observations in the training data.
+D=2; % Number of covariates.
+X = randn(n,k);
+xs = randn(1,k);
+sn = 0.1;
+hyp.lik = log(sn);
+noise = normrnd(0,sn,n,1);
+b0 = -k; % For this example, the constant of regression is just negative the number of covariates.
+y = b0 .* ones(n,1) + X * (1:k)' + noise; % Marginal effects are b1 = 1, b2 = 2, ..., bk = k
+train_X = normalize(X); % Normalize training data
+train_y = normalize(y); % Normalize training data
+meanfunc = {@meanZero};
+covfunc = {@covSEiso};
+ls = 1;
+sf = 1;
+hyp.cov = log([ ls sf ]);
+hyp.mean = [];
+%likfunc = {@likGauss};
+%inffunc = {}
+
+
+
+
+
+[ N, D ] = size(X);
+
+% For meanfunc = {@meanZero}
+% Partial E[ ys | X, y, xs] / Partial xs = Partial c(xs, X) / Partial xs * inv(C(X) + sn^2 I) * y
+
+% Define components
+
+% Define uninverted C(X) + sn^2 I
+Cs = (feval(covfunc{:}, hyp.cov, X) + exp(hyp.lik)^2 * eye(N));
+
+% Define xs - xi, a 1xD vec, make n x D
+dXs = zeros(N,D);
+for i = 1:N
+    dXs(i,:) = xs - X(i,:);
 end
 
-% Make the mean function
-pd_cxsX_dxs = -1/exp(hyp.cov(LS_SWITCH))^2 * (xs(d) - X(:,d)) .* feval(covfunc{:}, hyp.cov, X, xs);
-Cs = feval(covfunc{:}, hyp.cov, X) + exp(hyp.lik)^2 * eye(n);
-mean = pd_cxsX_dxs * Cs \ (y - feval(meanfunc{:}, hyp.mean, X));
+% zsa
 
-% Make the covariance function
+dc_xs_X_dxs = zeros(N,D);
+for i = 1:N
+    dc_xs_X_dxs(i,:) = (-1/exp(hyp.cov(1))^2) * (xs - X(i,:)) * feval(covfunc{:}, hyp.cov, X(i,:), xs);
+end
+dc_X_xs_dxs = -dc_xs_X_dxs';
+DxDterm2 = dc_xs_X_dxs * inv(Cs) * dc_X_xs_dxs;
 
+% specific to the covSEiso kernel
+% mean
+mean_vec = (-1/exp(hyp.cov(1))^2) * dXs' * (feval(covfunc{:}, hyp.cov, X, xs) .* (Cs \ y));
+% vcov
+DxDterm1 = ;
+var_mat = DxDterm1 + DxDterm2;
+
+vcov_me = 
+% For covfunc = {@covSEiso}
+
+
+
+ 
+% MODE = 'iso';
+% % Switch length scale for the derivative depending on if the kernel is SEiso or SEard
+% LS_SWITCH = 0;
+% if strcmp(MODE,'iso')
+%     LS_SWITCH = 1; % For ISO
+% elseif strcmp(MODE,'ard')
+%     LS_SWITCH = d;
+% end
+% 
+% % Make the mean function
+% pd_cxsX_dxs = -1/exp(hyp.cov(LS_SWITCH))^2 * (xs(d) - X(:,d)) .* feval(covfunc{:}, hyp.cov, X, xs);
+% Cs = feval(covfunc{:}, hyp.cov, X) + exp(hyp.lik)^2 * eye(n);
+% mean = pd_cxsX_dxs * Cs \ (y - feval(meanfunc{:}, hyp.mean, X));
+% 
+
+
+end
