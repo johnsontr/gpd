3438653660b5bdc37372eb76051f16b3ca6a2b13
T. Ryan Johnson <johnsontr@gmail.com>
2022-12-08 18:38:19 -0600

    Update me.m
    Rebuild me(). Simplify switch between covSEiso and covSEard. Change d_c_xs_X_dxs to d_c_X_xs_dxs. Instead of building d_c_xs_X_dxs by column, build it all at once.

--- me.m
+++ me.m
@@ -1,4 +1,4 @@
-function [ mean_vec, diag_var_mat ] = me(hyp, meanfunc, covfunc, X, y, xs)
+function [ mean_vec, var_vec ] = me(hyp, meanfunc, covfunc, X, y, xs)
 % Calculate the estimated marginal effect and variance for a specific test
 % point xs. This is a support function for pme.
 %       nargin 5 - Generate estimates with w.r.t. the training sample.
@@ -8,50 +8,56 @@ function [ mean_vec, diag_var_mat ] = me(hyp, meanfunc, covfunc, X, y, xs)
 % processes, the meanfunc function input is unused. The function input is 
 % kept as a placeholder for when support is added for additional mean
 % functions in the future.
+    
+    % First, make components common to either covfunc
 
     [ N, D ] = size(X);                                                 % [number of observations in the sample, number of covariates]
-    
-    % Make the diagonal matrix of the length scales, the form of which 
-    % depends on whether the covariance function is covSEiso or covSEard.
-    switch str2num(feval(covfunc{:}))
-        case 2                                                          % If covfunc requires two inputs, then it's covSEiso
-            Lambda = diag(repmat(exp(hyp.cov(1))^2,D,1)); 
-        otherwise                                                       % If it's not covSEiso, then it's covSEard
-            Lambda = diag((exp(hyp.cov(1:D)).^2));
-    end
-    
-    % Make components used for calculation.
-    Cs = (feval(covfunc{:}, hyp.cov, X) + exp(hyp.lik)^2 * eye(N));     % Define uninverted C(X,X) + sn^2 I for later use.
 
+    Cs = (feval(covfunc{:}, hyp.cov, X) + exp(hyp.lik)^2 * eye(N));     % C(X,X) + sn^2 I    
+
+    % Define the x-specific product-rule factor created from
+    % differentiating a squared exponential kernel. This is common to both
+    % covSEiso and covSEard
     dXs = zeros(N,D);                                                   % Preallocate
     for i = 1:N
         dXs(i,:) = xs - X(i,:);                                         % xs - xi is a 1xD vec
     end
-    
-    % Make partial derivative c(xs,X) partial xs, which is DxN
-    d_c_xs_X_dxs = zeros(D,N);                                          % Preallocate
-    for i = 1:N
-        d_c_xs_X_dxs(:,i) = -(Lambda^-1) * (xs' - X(i,:)') * feval(covfunc{:}, hyp.cov, X(i,:), xs);
-    end
-    
-    %
-    %
-    % Make the mean function and the covariance function from the components defined above.
-    %
-    %
 
-    % mean function
-    mean_vec = - Lambda^-1 * dXs' * (feval(covfunc{:}, hyp.cov, X, xs) .* (Cs \ y));
+    % Diagonal matrix of the length scales and the variance matrix of
+    % marginal effects depend on whether the covariance function is covSEiso or covSEard.
+    switch str2num(feval(covfunc{:}))
 
-    % vcov function
-    if str2num(feval(covfunc{:})) == 2
-        var_mat = Lambda^-1 * exp(hyp.cov(2))^2 - d_c_xs_X_dxs * inv(Cs) * (-d_c_xs_X_dxs');
-    else % If it's not covSEiso, then it's covSEard
-        var_mat = Lambda^-1 * exp(hyp.cov(D+1))^2 - d_c_xs_X_dxs * inv(Cs) * (-d_c_xs_X_dxs');
+        case 2                                                          % If covfunc requires two inputs, then it's covSEiso
+
+            % Diagonal matrix of the length scales
+            Lambda = diag(repmat(exp(hyp.cov(1))^2,D,1)); 
+
+            % Make partial derivative of c(X,xs) w.r.t. xs, which is NxD
+            d_c_X_xs_dxs = -Lambda^-1 * dXs' * (feval(covfunc{:}, hyp.cov, X, xs)); 
+
+            % Store the scale factor specific to the covariance function
+            cov_scale_factor = exp(hyp.cov(2))^2;
+
+        otherwise                                                       % If it's not covSEiso, then it's covSEard
+
+            % Diagonal matrix of the length scales
+            Lambda = diag((exp(hyp.cov(1:D)).^2)); 
+
+            % Make partial derivative of c(X,xs) w.r.t. xs, which is NxD
+            d_c_X_xs_dxs = -Lambda^-1 * dXs' * (feval(covfunc{:}, hyp.cov, X, xs)); 
+
+            % Store the scale factor specific to the covariance function
+            cov_scale_factor = exp(hyp.cov(D+1))^2;
+
     end
-    
-    % Only return the diagonals of var_mat, which corresponds to the
-    % variance of the marginal distribution of the marginal effects.
-    diag_var_mat = diag(var_mat);
+
+    % Marginal effect of the expected value of the Gaussian process at xs w.r.t. each covariate
+    mean_vec = d_c_X_xs_dxs' .* (Cs \ y); 
+
+    % Make the variance-covariance matrix of the marginal effects
+    var_mat = cov_scale_factor * Lambda^-1 - d_c_X_xs_dxs' * inv(Cs) * (-d_c_X_xs_dxs); 
+
+    % Only return the diagonals of var_mat, which corresponds to the variance of the marginal distribution of the marginal effects.
+    var_vec = diag(var_mat); 
 
 end
